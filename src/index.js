677bf1108b1d5a25afe1d5c923cfa028af5707e1
ybadr99 <yousefbadr909@gmail.com>
2023-05-03 02:17:47 +0200

    init commit / setup the project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-import _ from 'lodash';
 import './style.css';
-function component() {
-    const element = document.createElement('div');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+
+console.log('todolist');
