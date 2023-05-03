03cb444e123544133ebceb7b96342917c54da189
ybadr99 <yousefbadr909@gmail.com>
2023-05-03 23:08:54 +0200

    add index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './style.css';
+import {
+  addTodo, renderTodos,
+} from './functions.js';
+
+document.querySelector('.text-input').addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    addTodo(e.target.value);
+    renderTodos();
+    e.target.value = '';
+  }
+});
+
+renderTodos();
