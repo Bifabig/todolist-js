a8e30fd0158b74fa4e474e3010aea7f908af7d10
ybadr99 <yousefbadr909@gmail.com>
2023-05-03 23:07:51 +0200

    fix linters

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,4 @@
-import { getTodos, save } from './data';
+import { getTodos, save } from './data.js';
 
 const todos = getTodos();
 
@@ -10,35 +10,32 @@ export const addTodo = (desc) => {
 export const deleteTodo = (id) => {
   const i = todos.findIndex((todo) => todo.id === id);
   if (i > -1) todos.splice(i, 1);
-  reoderArrIndexes()
+  // reOrderIndexes...
+  todos.forEach((a, i) => {
+    a.id = i;
+  });
+  // save
   save(todos);
 };
 
 export const editTodo = (id, desc) => {
   const todo = todos.find((todo) => todo.id === id);
-  console.log(todo);
   todo.desc = desc;
   save(todos);
 };
 
-const reoderArrIndexes = () => todos.forEach((a, i)=>{
-    a.id = i
-})
-
-
 export const renderTodos = () => {
-    document.querySelector('.todos').innerHTML = '';
-    if (todos) {
-      todos.forEach((todo) => {
-        const todoEl = `<li class="todo">
+  document.querySelector('.todos').innerHTML = '';
+  if (todos) {
+    todos.forEach((todo) => {
+      const todoEl = `<li class="todo">
         <div> 
             <input class="completed-btn" type="checkbox"> 
             <p>${todo.desc}</p> 
         </div>
         <i class="uil uil-draggabledots"></i>
-        </li>`
-        document.querySelector('.todos').innerHTML += todoEl
-      });
-    }
-  };
-  
+        </li>`;
+      document.querySelector('.todos').innerHTML += todoEl;
+    });
+  }
+};
